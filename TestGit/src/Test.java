ebb0902fe6cf7ce0e821973619b8e0f0553574d0
wuxin <xixistdio@yeah.net>
2017-11-07 19:02:31 +0800

    add func

--- TestGit/src/Test.java
+++ TestGit/src/Test.java
@@ -1,4 +1,3 @@
-
 /**
  * @author Administrator
  *
@@ -6,5 +5,8 @@
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(" first git project ");
+		List<String> testList = new ArrayList<String>();
+		testList.add("wuxin");
+		testList.add("snowflake");
 	}
 }
